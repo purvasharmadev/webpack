cf744169f32687619af30e25861325d2c9cf655e
Purva sharma <purva08815294379@gmail.com>
2022-08-16 16:25:48 +0530

    styled the app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
 import './app.css'
+import logo from './logo.png'
+
 
 const App = () => {
   return `
+  <div class="container">
+  <img src="${logo}" alt="build icon" height="150px" width="150px" />
     <h1> Welcome </h1>
-    <p> Build apps with plain JS but with modern tooling  </p>
+    <h2> Build apps with plain JS but with modern tooling  </h2>
+</div>
     `
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import App from "./App";
-import logo from './logo.png'
 const root = document.getElementById("root");
 root.innerHTML = `
-<img src="${logo}" alt="build icon" height="300px" width="300px" />
 ${App()};
 `;
